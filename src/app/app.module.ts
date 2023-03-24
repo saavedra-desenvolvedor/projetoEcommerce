02e36a1b61376ce5c96af027c91e4a5f3642d396
saavedra-desenvolvedor <66433098+saavedra-desenvolvedor@users.noreply.github.com>
2023-03-24 13:27:33 -0300

    1 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,7 +10,7 @@ import { ProdutosComponent } from './produtos/produtos.component';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, FooterComponent, ProdutosComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule,FormsModule ],
   providers: [],
   bootstrap: [AppComponent],
 })
