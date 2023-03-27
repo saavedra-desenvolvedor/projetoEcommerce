5b758bbb45803fc5d3cd8ae8ac58115adfbb22e9
saavedra-desenvolvedor <66433098+saavedra-desenvolvedor@users.noreply.github.com>
2023-03-27 14:08:26 -0300

    adicionando imagens do produto

--- src/app/produtos/produtos.component.ts
+++ src/app/produtos/produtos.component.ts
@@ -1,17 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduto, produtos } from '../produtos';
 
-
 @Component({
   selector: 'app-produtos',
   templateUrl: './produtos.component.html',
-  styleUrls: ['./produtos.component.scss']
+  styleUrls: ['./produtos.component.scss'],
 })
 export class ProdutosComponent implements OnInit {
- produtos: IProduto[] = produtos;
-  constructor() { }
+  produtos: IProduto[] = produtos;
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
