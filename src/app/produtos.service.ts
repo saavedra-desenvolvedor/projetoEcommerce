e3d73920b71702bcd1fdc95a7c1d3f65ec235298
saavedra-desenvolvedor <66433098+saavedra-desenvolvedor@users.noreply.github.com>
2023-03-29 13:32:16 -0300

    corrigindo erros do produto service

--- src/app/produtos.service.ts
+++ src/app/produtos.service.ts
@@ -1,15 +1,17 @@
-import { Injectable } from '@angular/core';
-import { IProduto, produtos } from './produtos';
+import { Injectable } from '@angular/core';
+import { IProduto, produtos } from './produtos';
 
-@Injectable()
-export class ProdutosService {
-  produtos: IProduto[] = produtos;
-  constructor() {}
+@Injectable({
+  providedIn: 'root'
+})
+export class ProdutosService {
+  produtos: IProduto[] = produtos;
+  constructor() {}
 
-  getAll() {
-    return this.produtos;
-  }
-  getOne(produtoId: number) {
-    return this.produtos.find((produto) => (produto.id = produtoId));
-  }
+  getAll() {
+    return this.produtos;
+  }
+  getOne(produtoId: number) {
+    return this.produtos.find((produto) => (produto.id = produtoId));
+  }
 }
