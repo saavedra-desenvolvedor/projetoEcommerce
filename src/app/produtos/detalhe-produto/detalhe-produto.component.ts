00810ce6351aa3d40e758ef9c86b6a5aa96bcbd4
saavedra-desenvolvedor <66433098+saavedra-desenvolvedor@users.noreply.github.com>
2023-03-30 14:57:39 -0300

    2 files modified

--- src/app/produtos/detalhe-produto/detalhe-produto.component.ts
+++ src/app/produtos/detalhe-produto/detalhe-produto.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduto } from '../../produtos';
+import { ProdutosService } from '../../produtos.service';
 
 @Component({
-  selector: 'app-detalhe-produto',
-  templateUrl: './detalhe-produto.component.html',
-  styleUrls: ['./detalhe-produto.component.css']
+  selector: 'app-detalhe-produto',
+  templateUrl: './detalhe-produto.component.html',
+  styleUrls: ['./detalhe-produto.component.css'],
 })
-export class DetalheProdutoComponent implements OnInit {
+export class DetalheProdutoComponent implements OnInit {
+  produto: IProduto | undefined;
+  constructor(
+    private produtosService: ProdutosService,
+    private route : ActivatedRoute,
+    ) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {
+    const routeParams = this.route.snapshot.paramMap;
+    const produtoId = Number(routeParams.get("id"));
+    this.produto = this.produtosService.getOne(produtoId)
+  }
 }
--- src/app/produtos/produtos.module.ts
+++ src/app/produtos/produtos.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ProdutosRoutingModule } from './produtos-routing.module';
 
 import { DetalheProdutoComponent } from './detalhe-produto/detalhe-produto.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [DetalheProdutoComponent],
-  imports: [CommonModule, ProdutosRoutingModule],
+  imports: [CommonModule, ProdutosRoutingModule, FormsModule],
 })
 export class ProdutosModule {}
