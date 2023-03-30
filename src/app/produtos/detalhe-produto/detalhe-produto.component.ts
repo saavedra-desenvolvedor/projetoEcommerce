29247d9b09bea3ff6b3212bdb8017d4a66a2dd3d
saavedra-desenvolvedor <66433098+saavedra-desenvolvedor@users.noreply.github.com>
2023-03-30 15:41:20 -0300

    corrigigindo alguns erros em detalhe produto

--- src/app/produtos/detalhe-produto/detalhe-produto.component.ts
+++ src/app/produtos/detalhe-produto/detalhe-produto.component.ts
@@ -1,25 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { IProduto } from '../../produtos';
-import { ProdutosService } from '../../produtos.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NotificacaoService } from '../../notificacao.service';
+import { IProduto } from '../../produtos';
+import { ProdutosService } from '../../produtos.service';
 
 @Component({
-  selector: 'app-detalhe-produto',
-  templateUrl: './detalhe-produto.component.html',
-  styleUrls: ['./detalhe-produto.component.css'],
+  selector: 'app-detalhe-produto',
+  templateUrl: './detalhe-produto.component.html',
+  styleUrls: ['./detalhe-produto.component.css'],
 })
-export class DetalheProdutoComponent implements OnInit {
-  produto: IProduto | undefined;
-  quantidade = 1;
+export class DetalheProdutoComponent implements OnInit {
+  produto: IProduto | undefined;
+  quantidade = 1;
 
-  constructor(
-    private produtosService: ProdutosService,
-    private route : ActivatedRoute,
-    ) {}
+  constructor(
+    private produtosService: ProdutosService,
+    private route : ActivatedRoute,
+    private notificacaoService : NotificacaoService,
+    ) {}
 
-  ngOnInit() {
-    const routeParams = this.route.snapshot.paramMap;
-    const produtoId = Number(routeParams.get("id"));
-    this.produto = this.produtosService.getOne(produtoId)
-  }
+  ngOnInit() {
+    const routeParams = this.route.snapshot.paramMap;
+    const produtoId = Number(routeParams.get("id"));
+    this.produto = this.produtosService.getOne(produtoId)
+  }
+  adicionarAoCarrinho() {
+    this.notificacaoService.notificar("O produto foi adicionado ao carrinho")
+  }
 }
