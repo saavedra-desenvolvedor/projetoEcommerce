54898521d1cf1dae9944e2ffaa1e3f8e7eb7c38d
Jorge Saavedra <66433098+saavedra-desenvolvedor@users.noreply.github.com>
2023-03-31 18:57:31 +0000

    criando a pagina do carrinho

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 
-const routes: Routes = [{ path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
-{path: '',redirectTo:"produtos", pathMatch: "full" },
-{path:"**", component: PaginaNaoEncontradaComponent}
-];
+const routes: Routes = [{ path: 'carrinho', loadChildren: () => import('./carrinho/carrinho.module').then(m => m.CarrinhoModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
