045ba61e86e8efd24df4dd3e565e6be20bb3419e
saavedra-desenvolvedor <66433098+saavedra-desenvolvedor@users.noreply.github.com>
2023-04-19 09:38:22 -0300

    criando componentes da barra de pesquisa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,33 @@
+import { enableProdMode, NgModule } from '@angular/core';
+import { FormBuilder, FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { enableProdMode, NgModule } from '@angular/core';
-import { FormBuilder, FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 //import { ProdutosComponent } from './produtos/produtos.component';
-import { MatSnackBarModule } from "@angular/material/snack-bar";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BarraPesquisaComponent } from './barra-pesquisa/barra-pesquisa.component';
 enableProdMode();
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent,PaginaNaoEncontradaComponent ],
-  imports: [BrowserModule,BrowserAnimationsModule, AppRoutingModule,FormsModule, MatSnackBarModule ],
-  providers: [FormBuilder],
-  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PaginaNaoEncontradaComponent,
+    BarraPesquisaComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    MatSnackBarModule,
+  ],
+  providers: [FormBuilder],
+  bootstrap: [AppComponent],
 })
-export class AppModule {}
-
+export class AppModule {}
